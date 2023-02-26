9262ad2268b7ab80efbacfb233687fc93f8cd670
Freebo12 <Tumechko12@gmail.com>
2023-02-26 15:40:07 -0500

    Update StatisticsList.js

--- src/components/Statistics/StatisticsList.js
+++ src/components/Statistics/StatisticsList.js
@@ -11,7 +11,7 @@ import {
 export const StatisticsList = ({ title, items }) => {
   return (
     <SectionStatistics>
-      {title !== '' ? <Title>{title}</Title> : <Title>Upload More</Title>}
+      {title !== '' ? <Title>{title}</Title> : <></>}
 
       <ListStats>
         {items.map(item => (
